b626097a51303c2bb0297b99980b7b5b2d99d83c
Juju C <juliette.coustard@gmail.com>
2017-10-31 10:14:54 +0100

    JS: display random words

--- game.js
+++ game.js
@@ -2,6 +2,10 @@ const $playerZone = document.querySelector('.player-zone');
 const $wordZone = document.querySelector('.word-zone');
 let startedGame = false;
 
+// ----------------------------------------------------------------
+// ----------------------------- Game -----------------------------
+// ----------------------------------------------------------------
+
 document.addEventListener('keypress', function (event) {
     if(!startedGame) {
         startedGame = true;
@@ -11,3 +15,22 @@ document.addEventListener('keypress', function (event) {
         $playerZone.innerHTML += event.key;
     }
 });
+
+displayRandomWords();
+
+// ----------------------------------------------------------------
+// -------------------------- Functions ---------------------------
+// ----------------------------------------------------------------
+function displayRandomWords() {
+    setInterval(function() {
+        let word = chooseRandomWord();
+        $wordZone.innerHTML += `<p>${word}</p>`;
+    }, 1000);
+
+    clearInterval(interval);
+}
+
+function chooseRandomWord() {
+    let i = Math.floor(Math.random() * 100) % words.length;
+    return words[i];
+}
