dbf68b4b5d4e1bb911bdb4dcbda739130ee3d39d
Juju C <juliette.coustard@gmail.com>
2017-10-31 09:28:04 +0100

    GENERAL: added professor's files

---
+++ game.js
@@ -0,0 +1,6 @@
+const $playerZone = document.querySelector('.player-zone');
+const $wordZone = document.querySelector('.word-zone');
+
+document.addEventListener('keypress', function (event) {
+    // sur l'appuie d'une touche
+});
